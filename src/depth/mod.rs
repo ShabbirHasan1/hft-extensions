8adfc43171de4adea7517f5a0777b69d5945cd35
dongruoyu <satoshi996brk@gmail.com>
2024-06-29 02:30:12 +0800

    depth文件夹改为models

--- src/depth/mod.rs
+++
@@ -1,8 +0,0 @@
-use nalgebra::DMatrix;
-
-pub mod analyticmarketdepth;
-
-pub use analyticmarketdepth::AnalyticMarketDepth;
-
-const MAX_DEPTH: i32 = 5000_i32;
-type EvaluateMatrix = DMatrix<f32>;  // 5000*5的f32矩阵,列分别为price, qty, hit_prob, exp_rtn, exp_hit_time 
